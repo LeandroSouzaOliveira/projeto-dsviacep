e457055ad287a72a733d76f6839cf153f58431a3
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-08-09 14:45:34 -0300

    Event to include the number in the address object

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -31,9 +31,14 @@ export function init() {
     state.errorNumber = document.querySelector('[data-error="number"]');
 
     state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.inputNumber.addEventListener('keyup', handleInputNumberKeyUp);
     state.btnClear.addEventListener('click', handleBtnClearClick);
     state.btnSave.addEventListener('click', handleBtnSaveClick);
     state.inputCep.addEventListener('change', handleInputCepChange);
+}
+
+function handleInputNumberKeyUp(event) {
+    state.address.number = event.target.value;
 }
 
 async function handleInputCepChange(event) {
@@ -67,7 +72,7 @@ function handleInputNumberChange(event) {
 
 async function handleBtnSaveClick(event) {
     event.preventDefault();
-    console.log(event.target);
+    console.log(state.address);
 }
 
 function handleBtnClearClick(event) {
